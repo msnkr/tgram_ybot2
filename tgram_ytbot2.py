9739b6b193937dd395a9d1ff48f1e0f41c14236e
msnkr <mikyle.designs@gmail.com>
2022-11-16 10:17:32 +0200

    .

--- tgram_ytbot2.py
+++ tgram_ytbot2.py
@@ -1,14 +1,20 @@
-
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+files_end = ['.mp4', '.mp3', '.webm', '.part']
+def clear_downloads():
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='I am a bot. Type {/audio {artist song}} to search artist and song name audio. Type {/video {artist video}} to search artist and song video. You will recieve a message when the file is ready for download. Example {/video Joji Gimme Love}, {/audio Joji Gimme Love}. Press {/help} for a list of current problems. ')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Enter URL: ')
 
 
 def help(update, context):
@@ -17,7 +23,7 @@ def help(update, context):
 
 
 def main():
-    updater = Updater(token='TOKEN')
+    updater = Updater(token=os.getenv('TOKEN'))
     dispatcher = updater.dispatcher
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -25,10 +31,10 @@ def main():
     dispatcher.add_handler(start_handler)
 
     updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_handler(CommandHandler('audio', get_audio_url))
-    updater.dispatcher.add_handler(CommandHandler('video', get_video_url))
-    updater.dispatcher.add_handler(CommandHandler('da', get_audio))
-    updater.dispatcher.add_handler(CommandHandler('dv', get_video))
+    
+
 
     updater.start_polling()
     updater.idle()
+
+main()

92a730042716e569e8dffefecb83d25381189be8
msnkr <mikyle.designs@gmail.com>
2022-11-18 10:29:00 +0200

    Update tgram_ytbot2.py

--- tgram_ytbot2.py
+++ tgram_ytbot2.py
@@ -14,22 +14,57 @@ import youtube_dl
 load_dotenv('.env')
 files_end = ['.mp4', '.mp3', '.webm', '.part', '.mkv']
 
-def download_video(update: Update, context: CallbackContext):
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }]}
+    
+def download_audio(update: Update, context: CallbackContext):
+    for file in os.listdir(r'C:\Users\Digital\Documents\GitHub\tgram_ybot2'):
+        for item in files_end:
+            if file.endswith(item):
+                context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(f'{file}', 'rb'))
+
+
+def audio(update: Update, context: CallbackContext):
+    clear_downloads()
+    text = update.message.text.split(" ")
+    audio_text = text[1]
+    update.message.reply_text('Downloading audio now. Please wait..')
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+       ydl.download([audio_text])
+    video_download(update, context)
+
+
+
+def video_download(update: Update, context: CallbackContext):
     for file in os.listdir(r'C:\Users\Digital\Documents\GitHub\tgram_ybot2'):
         for item in files_end:
             if file.endswith(item):
                 context.bot.send_video(chat_id=update.effective_chat.id, video=open(f'{file}', 'rb'))
-                clear_downloads()
 
 
-def download(update: Update, context: CallbackContext):
+def video(update: Update, context: CallbackContext):
     clear_downloads()
     text = update.message.text.split(" ")
     video_text = text[1]
-    update.message.reply_text('Downloading Video now. Please wait..')
+    update.message.reply_text('Downloading video now. Please wait..')
+    with youtube_dl.YoutubeDL({'format': '18'}) as ydl:
+       ydl.download([video_text])
+    video_download(update, context)
+
+
+def short_video(update: Update, context: CallbackContext):
+    clear_downloads()
+    text = update.message.text.split(" ")
+    video_text = text[1]
+    update.message.reply_text('Downloading video now. Please wait..')
     with youtube_dl.YoutubeDL({'format': '22'}) as ydl:
        ydl.download([video_text])
-    update.message.reply_text('Your video has finished downloading. Send {/dv} to download your video. ')
+    video_download(update, context)
 
 
 def clear_downloads():
@@ -53,8 +88,9 @@ def main():
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_handler(CommandHandler('download', download))
-    updater.dispatcher.add_handler(CommandHandler('dv', download_video))
+    updater.dispatcher.add_handler(CommandHandler('s', short_video))
+    updater.dispatcher.add_handler(CommandHandler('v', video))
+    updater.dispatcher.add_handler(CommandHandler('a', audio))
     
     updater.start_polling()
     updater.idle()
